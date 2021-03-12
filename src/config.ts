ed09885770f3ff8bfc9255afc760c7864f4191d7
Joham Medina <joham.smc@gmail.com>
2021-03-11 22:52:58 -0500

    Change from local DB to remote DB(mongo atlas)

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 export default {
   MONGO_DATABASE: process.env.MONGO_DATABASE,
   MONGO_HOST: process.env.MONGO_HOST,
+  MONGO_USER: process.env.MONGO_USER,
+  MONGO_PASSWORD: process.env.MONGO_PASSWORD,
   APP_PORT: process.env.PORT || 3000,
 };
--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,19 @@
-import mongoose, { ConnectionOptions } from 'mongoose';
-import config from './config';
+import mongoose, { ConnectionOptions } from "mongoose";
+import config from "./config";
 
 (async () => {
   try {
-    mongoose.set('useCreateIndex', true);
+    mongoose.set("useCreateIndex", true);
     const options: ConnectionOptions = {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     };
-    await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, options);
-    console.log('Database is now!');
+    // await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, options);
+    await mongoose.connect(
+      `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@apirest.plgok.mongodb.net/${config.MONGO_DATABASE}?retryWrites=true&w=majority`,
+      options
+    );
+    console.log("Database is now!");
   } catch (error) {
     console.log(error);
   }
